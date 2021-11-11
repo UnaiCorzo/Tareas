1e0c45eb4ca550f5dc51ad270af24558f38bab79
Unai Corzo <ucorzoga20dw@ikzubirimanteo.com>
2021-11-11 13:16:10 +0100

    Add dropdown

--- resources/views/tareas.blade.php
+++ resources/views/tareas.blade.php
@@ -1,19 +1,17 @@
 @extends('index')
 
 @section('contenido')
-    <h3>Lista de tareas</h3>
-
     <form method="post" class="col-4 mt-4" action="/tarea">
         {{csrf_field()}}
         <p class="lead">Nueva tarea</p>
         <label><b>Tarea</b></label><br>
         <input type="text" name="nombre" class="form-control"><br>
-        <input type="submit" value="+ Añadir">
+        <button class="btn bg-secondary text-white" type="submit">+ Añadir</button>
     </form>
 
     <div class="mt-5">
         <p class="lead">Tareas actuales</p>
-        <label><b>Tarea</b></label><br>
+        <label class="mb-2"><b>Tarea</b></label><br>
         <table>
             @if (count($tareas) > 0)
                 @foreach ($tareas as $tarea)
