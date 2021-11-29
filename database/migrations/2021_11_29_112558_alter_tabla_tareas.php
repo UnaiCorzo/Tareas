ae365cd9ead88260bb86ef2bddae0d1d79c64338
Unai Corzo <ucorzoga20dw@ikzubirimanteo.com>
2021-11-29 12:34:41 +0100

    Create new migration for new attribute

---
+++ database/migrations/2021_11_29_112558_alter_tabla_tareas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTablaTareas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tareas', function (Blueprint $table) {
+            $table->unsignedBigInteger("usuario_id");
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
