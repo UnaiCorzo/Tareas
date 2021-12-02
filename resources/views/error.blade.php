16d704b7748aac66cc22715ed2ce48b731ef3b05
Unai Corzo <ucorzoga20dw@ikzubirimanteo.com>
2021-12-02 13:36:22 +0100

    Cleanup

--- resources/views/error.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('new')
-
-@section ('error')
-
-<span class="text-danger"><b>Error. Introduce un nombre y el usuario</b></span>
-
-@endsection
--- resources/views/error2.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('new')
-
-@section ('error2')
-
-<span class="text-danger"><b>Error. Introduce un nombre y el apellido</b></span>
-
-@endsection
